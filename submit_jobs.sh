dec953b8282cf486aa068c12eb7a74376f1c6274
Chara Kitsaki <ckitsaki@spar0105.usatlas.bnl.gov>
2022-02-09 04:04:39 -0500

    configs for plotting submission

--- submit_jobs.sh
+++ submit_jobs.sh
@@ -1,4 +1,6 @@
 #!/bin/sh
+
+# experimental systematics submission
 condor_submit ./submission_files/submit_diboson1.sub
 condor_submit ./submission_files/submit_diboson2.sub
 condor_submit ./submission_files/submit_Fakes.sub
@@ -11,3 +13,16 @@ condor_submit ./submission_files/submit_Vgamma.sub
 condor_submit ./submission_files/submit_vh.sub
 condor_submit ./submission_files/submit_WWEW.sub
 condor_submit ./submission_files/submit_Zjets.sub
+
+# plot production submission
+#condor_submit ./submission_files/plot_production/submit_plots_SR.sub
+#condor_submit ./submission_files/plot_production/submit_plots_SR_linear.sub
+#condor_submit ./submission_files/plot_production/submit_plots_DYCR.sub
+#condor_submit ./submission_files/plot_production/submit_plots_TopCR.sub
+#condor_submit ./submission_files/plot_production/submit_plots_WWCR.sub
+#condor_submit ./submission_files/plot_production/submit_plots_ggFCR1.sub
+#condor_submit ./submission_files/plot_production/submit_plots_ggFCR2.sub
+#condor_submit ./submission_files/plot_production/submit_plots_ggFCR3.sub
+
+#condor_submit ./submission_files/plot_production/submit_plots_single.sub
+#condor_submit ./submission_files/plot_production/submit_plots_paper.sub
---
+++ submit_plots.sh
@@ -0,0 +1,44 @@
+#!/bin/sh
+export ATLAS_LOCAL_ROOT_BASE=/cvmfs/atlas.cern.ch/repo/ATLASLocalRootBase
+source ${ATLAS_LOCAL_ROOT_BASE}/user/atlasLocalSetup.sh 
+setupATLAS
+export ALRB_rootVersion=6.20.06-x86_64-centos7-gcc8-opt
+lsetup root 
+
+ulimit -n 2048
+# choose among SR, WWCR, TopCR, DYCR, ggFCR1, ggFCR2, ggFCR3
+unblind=true # lep0_pt, lep1_pt, Ptll, lep0_eta, lep1_eta, Mll, nJets, jet0_pt, jet1_pt, jet2_pt, Mjj, jet0_eta, jet1_eta, Ml0j0, Ml0j1, Ml1j0, Ml1j1, MET, MT, DYll, DYjj, DPhill, DPhijj, SignedDPhijj, costhetastar, ptTot, pt_H, sumOfCentralitiesL
+forPaper=false
+addTheo=false
+lxplus=false
+
+region=$1
+observable=$2
+y_min=$3
+y_max=$4
+x_min=$5
+x_max=$6
+y_ratio_min=$7
+y_ratio_max=$8
+dir=/usatlas/u/ckitsaki/usatlasdata/supNote/vbf_differential_plotting/src
+cd $dir
+echo '==== plot production ===='
+root -b -q -l "plotting.C(\"$region\", \"$observable\", $unblind, $forPaper, $addTheo, $lxplus, $y_min, $y_max, \"$x_min\", $x_max, \"$y_ratio_min\", $y_ratio_max)"
+
+
+#lxplus=false
+
+#region1=$1
+#region2=$2
+#observable=$3
+#y_min=$4
+#y_max=$5
+#x_min=$6
+#x_max=$7
+#y_ratio_min=$8
+#y_ratio_max=$9
+
+#dir=/usatlas/u/ckitsaki/usatlasdata/supNote/vbf_differential_plotting/src
+#cd $dir
+#echo '==== plot production ===='
+#root -b -q -l "plotting.C(\"$region1\", \"$region2\", \"$observable\", $lxplus, $y_min, $y_max, \"$x_min\", $x_max, \"$y_ratio_min\", $y_ratio_max)"
