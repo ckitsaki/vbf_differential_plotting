22e9aad1b8b895832935448831d01f5b45122326
Chara Kitsaki <ckitsaki@spar0102.usatlas.bnl.gov>
2022-09-12 12:17:22 -0400

    update breakdown_sys.py

--- python/syst_breakdown.py
+++ python/syst_breakdown.py
@@ -4,6 +4,15 @@ import ROOT
 from ROOT import *
 import argparse
 import numpy as np
+# In order to use this script set up a virtual environment.
+# $ mkdir venv
+# $ python3 -m vevn venv
+# $ source venv/bin/activate
+# $ python3 -m pip install astropy
+# $ python3 -m pip install pyroot
+# $ setupATLAS
+# $ lsetup "root 6.26.04-x86_64-centos7-gcc11-opt"
+
 
 def setXaxisTitle(observable):
 	xaxistitle = ''
@@ -13,16 +22,32 @@ def setXaxisTitle(observable):
 		xaxistitle = '#it{#Delta y}_{ll}'
 	if(observable=="DYjj"):
 		xaxistitle = '#it{#Delta y}_{jj}'
+	if(observable=="DPhill"):
+		xaxistitle = '#it{#Delta#phi}_{ll} [rad]'
 	if(observable=="Mll"):
-		xaxistitle = '#it{m}_{ll}'
+		xaxistitle = '#it{m}_{ll} [GeV]'
 	if(observable=='jet0_pt'):
-		xaxistitle = '#it{p}_{T}^{lead jet}'
+		xaxistitle = '#it{p}_{T}^{jet_{0}} [GeV]'
+	if(observable=='jet1_pt'):
+		xaxistitle = '#it{p}_{T}^{jet_{1}} [GeV]'
+	if(observable=='lep0_pt'):
+		xaxistitle = '#it{p}_{T}^{lep_{0}} [GeV]'
+	if(observable=='lep1_pt'):
+		xaxistitle = '#it{p}_{T}^{lep_{1}} [GeV]'
+	if(observable=='pt_H'):
+		xaxistitle = '#it{p}_{T}^{H} [GeV]'
+	if(observable=='Ptll'):
+		xaxistitle = '#it{p}_{T}^{ll} [GeV]'
+	if(observable=='costhetastar'):
+		xaxistitle = '#it{cos}#theta_{#eta}*'
+	if(observable=='SignedDPhijj'):
+		xaxistitle = '#it{#Delta#phi}_{jj} [rad]'
 	return xaxistitle
 
 def main(args):
-	gROOT.LoadMacro("./Style/AtlasStyle.C")
-	gROOT.LoadMacro("./Style/AtlasUtils.C")
-	gROOT.LoadMacro("./Style/AtlasLabels.C")
+	gROOT.LoadMacro("../src/Style/AtlasStyle.C")
+	gROOT.LoadMacro("../src/Style/AtlasUtils.C")
+	gROOT.LoadMacro("../src/Style/AtlasLabels.C")
 	SetAtlasStyle()
 	gROOT.SetBatch(True)
 
@@ -57,23 +82,25 @@ def main(args):
 			line.append(iline.strip().split())
 	bins_c = []
 	#print(line[0][1])
-	data = 'XSPlot_Data_Mjj.root'
+	data = '/usatlas/u/sagar/usatlaswork/scripts/XSPlotsChara/HistMergingGraphs/XSPlot_Data_' + str(observable) + '.root'
 	file = TFile(data,"READ")
 	obj_statOnly = observable + '_StatOnly'
 	statOnly = file.Get(obj_statOnly)
 	obj_total = observable + '_StatSys'
 	statSys = file.Get(obj_total)
 	
-	for ibin in range(statSys.GetN()):
+	for ibin in range(statSys.GetN()+1):
 		bins_c.append(float(line[0][ibin+1]))
 
 	my_array = np.array(bins_c)
-	print(bins_c)
-	h_totUnc = TH1D("h_totUnc", "h_totUnc", bins-1, np.array(bins_c))
+	h_totUnc = TH1D("h_totUnc", "h_totUnc", bins, np.array(bins_c))
+	h_statUnc = TH1D("h_statUnc", "h_statUnc", bins, np.array(bins_c))
+	print(bins)
 	for ibin in range(statSys.GetN()):
 		#print(statOnly.GetPointY(ibin))
-		#print(statSys.GetErrorY(ibin)/statOnly.GetPointY(ibin))
+		#rint(statSys.GetErrorY(ibin)/statOnly.GetPointY(ibin))
 		h_totUnc.SetBinContent(ibin+1, statSys.GetErrorY(ibin)/statOnly.GetPointY(ibin)*100)
+		h_statUnc.SetBinContent(ibin+1, statOnly.GetErrorY(ibin)/statOnly.GetPointY(ibin)*100)
 
 	print(bins_c)
 	table_file = 'ws_noGGF2_' + observable + '_Data.root_data_breakdown_reverse_rel.tex'
@@ -103,7 +130,7 @@ def main(args):
 		top_model.append(column[8])
 		ggf_model.append(column[9])
 		b_tag.append(column[3])
-		mcstat.append(column[0])
+		#mcstat.append(column[0])
 		#totunc.append(column[12])
 
 	for elem in range(len(lumi)):
@@ -118,7 +145,7 @@ def main(args):
 		newtop_model = str(top_model[elem]).replace('$','').replace('<','')
 		newggf_model = str(ggf_model[elem]).replace('$','').replace('<','')
 		newbtag = str(b_tag[elem]).replace('$','').replace('<','')
-		newmcstat = str(mcstat[elem]).replace('$','').replace('<','')
+		#newmcstat = str(mcstat[elem]).replace('$','').replace('<','')
 		#newtotunc = str(totunc[elem]).replace('$','').replace('<','')
 
 		lumi[elem] = newlumi
@@ -132,19 +159,19 @@ def main(args):
 		top_model[elem] = newtop_model
 		ggf_model[elem] = newggf_model
 		b_tag[elem] = newbtag
-		mcstat[elem] = newmcstat
+		#mcstat[elem] = newmcstat
 		#totunc[elem] = newtotunc
 
 	canvas = TCanvas("canvas","canvas",600,600)
-	h_lumi = TH1D("lumi", "lumi",  bins-1, np.array(bins_c))
-	h_fakes = TH1D("fakes", "fakes",  bins-1, np.array(bins_c))
-	h_signal_model = TH1D("sigmodel", "sigmodel",  bins-1, np.array(bins_c))
-	h_signal_pshower = TH1D("h_signal_pshower", "h_signal_pshower", bins-1, np.array(bins_c))
-	h_lepton_model = TH1D("h_lepton_model", "h_lepton_model", bins-1, np.array(bins_c))
-	h_jetmet = TH1D("h_jetmet", "h_jetmet", bins-1, np.array(bins_c))
-	h_bkg_model = TH1D("h_bkg_model", "h_bkg_model", bins-1, np.array(bins_c))
-	h_btag = TH1D("h_btag", "h_btag",  bins-1, np.array(bins_c))
-	h_MCstat = TH1D("h_MCstat", "h_MCstat",  bins-1, np.array(bins_c))
+	h_lumi = TH1D("lumi", "lumi",  bins, np.array(bins_c))
+	h_fakes = TH1D("fakes", "fakes",  bins, np.array(bins_c))
+	h_signal_model = TH1D("sigmodel", "sigmodel",  bins, np.array(bins_c))
+	h_signal_pshower = TH1D("h_signal_pshower", "h_signal_pshower", bins, np.array(bins_c))
+	h_lepton_model = TH1D("h_lepton_model", "h_lepton_model", bins, np.array(bins_c))
+	h_jetmet = TH1D("h_jetmet", "h_jetmet", bins, np.array(bins_c))
+	h_bkg_model = TH1D("h_bkg_model", "h_bkg_model", bins, np.array(bins_c))
+	h_btag = TH1D("h_btag", "h_btag",  bins, np.array(bins_c))
+	#h_MCstat = TH1D("h_MCstat", "h_MCstat",  bins-1, np.array(bins_c))
 	
 
 	for ibin in range(len(lumi)):
@@ -154,18 +181,25 @@ def main(args):
 		h_signal_pshower.SetBinContent(ibin+1, float(signal_pshower[ibin]))
 		h_lepton_model.SetBinContent(ibin+1, float(lepton_model[ibin]))
 		h_jetmet.SetBinContent(ibin+1, float(jetmet[ibin]))
-		h_bkg_model.SetBinContent(ibin+1, TMath.Sqrt( float(zjets_model[ibin]) + float(diboson_model[ibin]) + float(top_model[ibin]) + float(ggf_model[ibin])))
+		h_bkg_model.SetBinContent(ibin+1, TMath.Sqrt( float(zjets_model[ibin])*float(zjets_model[ibin]) + float(diboson_model[ibin])*float(diboson_model[ibin]) + float(top_model[ibin])*float(top_model[ibin]) + float(ggf_model[ibin])*float(ggf_model[ibin])))
 		h_btag.SetBinContent(ibin+1, float(b_tag[ibin]))
-		h_MCstat.SetBinContent(ibin+1, float(mcstat[ibin]))
+		#h_MCstat.SetBinContent(ibin+1, float(mcstat[ibin]))
 		#h_totUnc.SetBinContent(ibin+1, float(totunc[ibin]))
-
-	h_lumi.GetYaxis().SetRangeUser(0,ymax)
+	if(logY==True):
+		h_lumi.GetYaxis().SetRangeUser(1e+00,ymax)
+	else:
+		h_lumi.GetYaxis().SetRangeUser(0,ymax)
+	#h_lumi.SetMinimum(1e+00)
+	#h_lumi.SetMaximum(ymax)
 	h_lumi.GetYaxis().SetTitle("Relative Uncertainty [%]")
 	h_lumi.GetXaxis().SetTitle(setXaxisTitle(observable))
 	h_lumi.GetYaxis().SetLabelSize(.035)
 	h_lumi.GetXaxis().SetLabelSize(.035)
 	h_lumi.GetXaxis().SetTitleSize(.035)
 	h_lumi.GetYaxis().SetTitleSize(.035)
+	h_lumi.GetXaxis().SetLabelOffset(.01)
+	h_lumi.GetYaxis().SetLabelOffset(.01)
+	h_lumi.GetYaxis().SetTitleOffset(1.5)
 
 	h_lumi.SetLineColor(peelOrange)
 	h_fakes.SetLineColor(kCyan)
@@ -175,10 +209,13 @@ def main(args):
 	h_jetmet.SetLineColor(darkGreen)
 	h_bkg_model.SetLineColor(kMagenta-5)#(kTeal-8)
 	h_btag.SetLineColor(lightAtlasBlue)
-	h_MCstat.SetLineColor(kTeal-8)
+	#h_MCstat.SetLineColor(kGray+2)
+	h_statUnc.SetLineColor(kGray+2)
 	h_totUnc.SetLineColor(kBlack)
 	h_totUnc.SetLineStyle(kDashed)
 
+	if(logY==True):
+		gPad.SetLogy()
 	h_lumi.Draw()
 	h_fakes.Draw("same")
 	h_signal_model.Draw("same")
@@ -187,14 +224,94 @@ def main(args):
 	h_jetmet.Draw("same")
 	h_bkg_model.Draw("same")
 	h_btag.Draw("same")
-	h_MCstat.Draw("same")
+	#h_MCstat.Draw("same")
+	h_statUnc.Draw("same")
 	h_totUnc.Draw("same")
 
-	legend = TLegend(0.62,0.6,.92,0.8);
+	latex = TLatex()
+	if(observable=="Mjj"):
+		h_lumi.GetXaxis().ChangeLabel(6,-1,0,-1,-1,-1,"")
+		h_lumi.GetXaxis().ChangeLabel(5,-1,0,-1,-1,-1,"")
+		if(logY==True):
+			latex.DrawLatex(3050,5.5e-01,"#scale[.7]{6000}")
+		else: 
+			latex.DrawLatex(3050,-20.5,"#scale[.7]{6000}")
+	if(observable=="Mll"):
+		h_lumi.GetXaxis().ChangeLabel(10,-1,0,-1,-1,-1,"")
+		h_lumi.GetXaxis().ChangeLabel(9,-1,0,-1,-1,-1,"")
+		h_lumi.GetXaxis().ChangeLabel(8,-1,0,-1,-1,-1,"")
+		h_lumi.GetXaxis().ChangeLabel(7,-1,0,-1,-1,-1,"")
+		h_lumi.GetXaxis().ChangeLabel(6,-1,0,-1,-1,-1,"")
+		if(logY==True):
+			latex.DrawLatex(98,5.5e-01,"#scale[.9]{#infty}")
+		else: 
+			latex.DrawLatex(98,-20.5,"#scale[.9]{#infty}")
+	if(observable=="Ptll"):
+		h_lumi.GetXaxis().ChangeLabel(12,-1,0,-1,-1,-1,"")
+		h_lumi.GetXaxis().ChangeLabel(11,-1,0,-1,-1,-1,"")
+		h_lumi.GetXaxis().ChangeLabel(10,-1,0,-1,-1,-1,"")
+		h_lumi.GetXaxis().ChangeLabel(9,-1,0,-1,-1,-1,"")
+		if(logY==True):
+			latex.DrawLatex(220,6.5e-01,"#scale[.7]{1000}")
+		else: 
+			latex.DrawLatex(220,-20.5,"#scale[.7]{1000}")
+	if(observable=="jet0_pt"):
+		h_lumi.GetXaxis().ChangeLabel(8,-1,0,-1,-1,-1,"")
+		h_lumi.GetXaxis().ChangeLabel(7,-1,0,-1,-1,-1,"")
+		h_lumi.GetXaxis().ChangeLabel(6,-1,0,-1,-1,-1,"")
+		h_lumi.GetXaxis().ChangeLabel(5,-1,0,-1,-1,-1,"")
+		if(logY==True):
+			latex.DrawLatex(410,6.5e-01,"#scale[.7]{700}")
+		else: 
+			latex.DrawLatex(410,-20.5,"#scale[.7]{700}")
+	if(observable=="jet1_pt"):
+		h_lumi.GetXaxis().ChangeLabel(8,-1,0,-1,-1,-1,"")
+		h_lumi.GetXaxis().ChangeLabel(7,-1,0,-1,-1,-1,"")
+		h_lumi.GetXaxis().ChangeLabel(6,-1,0,-1,-1,-1,"")
+		if(logY==True):
+			latex.DrawLatex(175,6.e-01,"#scale[.7]{350}")
+		else: 
+			latex.DrawLatex(175,-20.5,"#scale[.7]{350}")
+	if(observable=="lep0_pt"):
+		h_lumi.GetXaxis().ChangeLabel(8,-1,0,-1,-1,-1,"")
+		h_lumi.GetXaxis().ChangeLabel(7,-1,0,-1,-1,-1,"")
+		h_lumi.GetXaxis().ChangeLabel(6,-1,0,-1,-1,-1,"")
+		h_lumi.GetXaxis().ChangeLabel(5,-1,0,-1,-1,-1,"")
+		if(logY==True):
+			latex.DrawLatex(185,6.e-01,"#scale[.7]{350}")
+		else: 
+			latex.DrawLatex(185,-20.5,"#scale[.7]{350}")
+	if(observable=="lep1_pt"):
+		h_lumi.GetXaxis().ChangeLabel(9,-1,0,-1,-1,-1,"")
+		h_lumi.GetXaxis().ChangeLabel(8,-1,0,-1,-1,-1,"")
+		h_lumi.GetXaxis().ChangeLabel(7,-1,0,-1,-1,-1,"")
+		h_lumi.GetXaxis().ChangeLabel(6,-1,0,-1,-1,-1,"")
+		h_lumi.GetXaxis().ChangeLabel(5,-1,0,-1,-1,-1,"")
+		if(logY==True):
+			latex.DrawLatex(97,6.e-01,"#scale[.7]{200}")
+		else: 
+			latex.DrawLatex(97,-20.5,"#scale[.7]{200}")
+	if(observable=="pt_H"):
+		h_lumi.GetXaxis().ChangeLabel(9,-1,0,-1,-1,-1,"")
+		h_lumi.GetXaxis().ChangeLabel(8,-1,0,-1,-1,-1,"")
+		h_lumi.GetXaxis().ChangeLabel(7,-1,0,-1,-1,-1,"")
+		if(logY==True):
+			latex.DrawLatex(380,6.e-01,"#scale[.7]{850}")
+		else: 
+			latex.DrawLatex(380,-20.5,"#scale[.7]{850}")
+	if(observable=="costhetastar"):
+		h_lumi.GetXaxis().ChangeLabel(7,-1,0,-1,-1,-1,"")
+		h_lumi.GetXaxis().ChangeLabel(6,-1,0,-1,-1,-1,"")
+		h_lumi.GetXaxis().ChangeLabel(5,-1,0,-1,-1,-1,"")
+		if(logY==True):
+			latex.DrawLatex(0.595,6.5e-01,"#scale[.7]{1}")
+		else: 
+			latex.DrawLatex(0.6,-20.5,"#scale[.7]{1}")
+	legend = TLegend(0.2,0.6,.92,0.85);
 	legend.SetBorderSize(0)
 	legend.SetFillStyle(0)
 	legend.SetTextSize(0.)
-	#legend.SetNColumns(2)
+	legend.SetNColumns(2)
 	legend.AddEntry(h_signal_model, '#bf{Signal modelling}', 'l')
 	legend.AddEntry(h_signal_pshower, '#bf{Signal parton shower}', 'l')
 	legend.AddEntry(h_bkg_model, '#bf{Background modelling}', 'l')
@@ -203,19 +320,25 @@ def main(args):
 	legend.AddEntry(h_btag, '#bf{b-tagging}', 'l')
 	legend.AddEntry(h_lepton_model, '#bf{Leptons}', 'l')
 	legend.AddEntry(h_lumi, '#bf{Luminosity}', 'l')
-	legend.AddEntry(h_MCstat, '#bf{MC statistics}', 'l')
+	legend.AddEntry(h_statUnc, '#bf{MC statistics}', 'l')
 	legend.AddEntry(h_totUnc, '#bf{Tot. Uncertainty}', 'l')
 	legend.Draw()
 
 	atlastex = TLatex()
 	atlastex.SetTextSize(.04)
-	print(bins_c[0])
-	atlastex.DrawLatex( bins_c[0] + 0.3*bins_c[0], 0.9*ymax, "#it{#bf{#scale[1.2]{ATLAS}}}  Internal")
-	atlastex.DrawLatex(bins_c[bins-1] - 0.3*bins_c[bins-1], 0.9*ymax, "#sqrt{#scale[.8]{s}} #scale[.8]{= 13 TeV,} #scale[.8]{139 fb^{-1}}")
+	#print(bins_c[0])
+	if(logY==True):
+	#	atlastex.DrawLatex( bins_c[0] + 0.7*bins_c[0], 0.3*ymax, "#it{#bf{#scale[1.2]{ATLAS}}}  Internal")
+	#	atlastex.DrawLatex(bins_c[bins] - .7*bins_c[bins-1], 0.3*ymax, "#sqrt{#scale[.8]{s}} #scale[.8]{= 13 TeV,} #scale[.8]{139 fb^{-1}}")
+		atlastex.DrawLatex( 2.5, 0.4*ymax, "#it{#bf{#scale[1.2]{ATLAS}}}  Internal")
+		atlastex.DrawLatex(6.2, 0.4*ymax, "#sqrt{#scale[.8]{s}} #scale[.8]{= 13 TeV,} #scale[.8]{139 fb^{-1}}")
+	else :
+		atlastex.DrawLatex( bins_c[0] + 0.3*bins_c[0], 0.9*ymax, "#it{#bf{#scale[1.2]{ATLAS}}}  Internal")
+		atlastex.DrawLatex(bins_c[bins-1] - 0.1*bins_c[bins-1], 0.9*ymax, "#sqrt{#scale[.8]{s}} #scale[.8]{= 13 TeV,} #scale[.8]{139 fb^{-1}}")
 
 	print(lumi)
 	print(ggf_model)
-	canvas.SaveAs("mytest.pdf")
+	canvas.SaveAs(str(observable)+"_sys_breakdown.pdf")
 
 if __name__ == "__main__":
 	from argparse import ArgumentParser
@@ -223,9 +346,10 @@ if __name__ == "__main__":
 	parser.add_argument('-bins','--bins', type=int, default=5, help='number of differential bins')
 	parser.add_argument('-ymax','--ymax', type=float, default=400, help='ymax')
 	parser.add_argument('-observable', '--observable', type=str, default='Mjj', help='observable')
+	parser.add_argument('-logY', '--logY', type=bool, default=False, help='set yaxis in logarithmic scale')
 	args = parser.parse_args()
 	observable = args.observable
 	bins = args.bins
 	ymax = args.ymax
+	logY = args.logY
 	main(args);
-
