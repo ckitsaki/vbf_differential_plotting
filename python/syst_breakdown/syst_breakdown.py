bbc4ca4b3dbdc2c493fddb8c2ef79f72d2032784
Chara Kitsaki <chara.kitsaki@cern.ch>
2022-09-26 10:58:53 +0200

    syst_breakdown.py: add detailed command for each observable

--- python/syst_breakdown/syst_breakdown.py
+++ python/syst_breakdown/syst_breakdown.py
@@ -12,6 +12,19 @@ import numpy as np
 # $ python3 -m pip install pyroot
 # $ setupATLAS
 # $ lsetup "root 6.26.04-x86_64-centos7-gcc11-opt"
+# $ python3 sys_breakdown.py --observable SignedDPhijj --ymax 4e+03 --logY True --bins 4
+# $ python3 sys_breakdown.py --observable DPhijj --ymax 4e+03 --logY True --bins 4
+# $ python3 sys_breakdown.py --observable DYjj --ymax 4e+03 --logY True --bins 5
+# $ python3 sys_breakdown.py --observable Mjj --ymax 1e+05 --logY True --bins 5
+# $ python3 sys_breakdown.py --observable jet1_pt --ymax 2e+04 --logY True --bins 4
+# $ python3 sys_breakdown.py --observable jet0_pt --ymax 1e+04 --logY True --bins 4
+# $ python3 sys_breakdown.py --observable costhetastar --ymax 4e+03 --logY True --bins 5
+# $ python3 sys_breakdown.py --observable Mll --ymax 1e+05 --logY True --bins 4
+# $ python3 sys_breakdown.py --observable DPhill --ymax 4e+03 --logY True --bins 5
+# $ python3 sys_breakdown.py --observable lep1_pt --ymax 4e+03 --logY True --bins 4
+# $ python3 sys_breakdown.py --observable lep0_pt --ymax 3e+04 --logY True --bins 5
+# $ python3 sys_breakdown.py --observable Ptll --ymax 4e+03 --logY True --bins 4
+# $ python3 sys_breakdown.py --observable DYll --ymax 5e+04 --logY True --bins 4
 
 def setXaxisTitle(observable):
 	xaxistitle = ''
